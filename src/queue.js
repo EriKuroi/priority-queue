f14bf73da7fa1114654a11ceaf7716bf32b7e8ae
Eri <stayagolubcov@gmail.com>
2019-03-24 20:54:52 +0300

    Add code to queue.js

--- src/queue.js
+++ src/queue.js
@@ -2,23 +2,37 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
-
+		if (maxSize){
+			this.maxSize = maxSize;
+		}else this.maxSize = 30;
+		this.heap = new MaxHeap();
+		
 	}
 
 	push(data, priority) {
+		if (this.size() === this.maxSize){
+			 throw(Error);
+		}else {
+			this.heap.push(data, priority);
+			
+		}
 
 	}
 
 	shift() {
+		if(this.size() > 0){
+			return this.heap.pop();
+		}else throw(Error);
 
 	}
 
 	size() {
-
+		return this.heap.size();
 	}
 
 	isEmpty() {
-		
+		//console.log(this.size, (this.size === 0));
+		return (this.size() === 0);
 	}
 }
 
